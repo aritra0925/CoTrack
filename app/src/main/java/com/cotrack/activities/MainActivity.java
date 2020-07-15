0a227a38fb3421192b4929f75b7166141b371229
Das <734580@cognizant.com>
2020-07-16 04:08:12 +0530

    Pushing skeleton

---
+++ app/src/main/java/com/cotrack/activities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cotrack.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cotrack.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.background_preview);
+        //VideoView view = (VideoView) findViewById(R.id.videoView);
+        // initiate a video view
+        //view.setVideoURI(uri);
+        //view.start();
+        clickOnUserLogin();
+    }
+    private void clickOnUserLogin(){
+        Button b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserLogin.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/cotrack/activities/ViewActivity.java
@@ -0,0 +1,15 @@
+package com.cotrack.activities;
+
+import android.os.Bundle;
+
+import com.cotrack.R;
+
+public class ViewActivity extends UserLogin {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_bar1);
+    }
+
+}
