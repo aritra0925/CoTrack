e7eef3f8849eac61212af9f9a04e40422b996c45
aritra0925 <aritra0925@github.com>
2020-07-19 23:57:50 +0530

    Updating covid code

--- app/src/main/java/com/cotrack/activities/MainActivity.java
+++ app/src/main/java/com/cotrack/activities/MainActivity.java
@@ -2,37 +2,45 @@ package com.cotrack.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cotrack.R;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_login);
-        //Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.background_preview);
-        //VideoView view = (VideoView) findViewById(R.id.videoView);
-        // initiate a video view
-        //view.setVideoURI(uri);
-        //view.start();
-        //clickOnUserLogin();
-    }
-    /*private void clickOnUserLogin(){
-        Button b1 = findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, UserLogin.class));
-            }
-        });
+        setContentView(R.layout.activity_main);
 
-    }*/
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
