8973a8a266f8731853bd7c463525e7b94a1c39fd
Piu-1901 <sau.piu19@gmail.com>
2020-07-21 03:11:45 +0530

    Committing Chat

--- app/src/main/java/com/cotrack/fragments/ChatFragment.java
+++ app/src/main/java/com/cotrack/fragments/ChatFragment.java
@@ -7,10 +7,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.cotrack.R;
@@ -18,7 +22,13 @@ import com.cotrack.adaptors.MessageListAdapter;
 import com.cotrack.models.Message;
 import com.cotrack.models.User;
 import com.cotrack.utils.APIUtils;
-
+import com.cotrack.utils.WatsonUtils;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.assistant.v2.Assistant;
+import com.ibm.watson.assistant.v2.model.CreateSessionOptions;
+import com.ibm.watson.assistant.v2.model.MessageContext;
+import com.ibm.watson.assistant.v2.model.MessageOptions;
+import com.ibm.watson.assistant.v2.model.MessageResponse;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -35,6 +45,15 @@ public class ChatFragment extends Fragment {
     private MessageListAdapter mMessageAdapter;
     private static ChatFragment instance = null;
     View view;
+    private EditText input;
+    public static final String assistant_apikey = "XmOiu7Oe0Bl2s6cA-_WPXsRmaLFQTADvFRiDIpHLW-6i";
+    public static final String assistant_url = "https://api.us-south.assistant.watson.cloud.ibm.com/instances/3c53eaa3-e748-4047-8989-cc1ed18c9c2a/v2/assistants/f4315384-6a4f-428a-b328-129debd27469/sessions";
+    public static final String workspace_id = "f4315384-6a4f-428a-b328-129debd27469";
+    MessageContext context;
+    private Handler handler = new Handler();
+    public ArrayAdapter<String> msgList;
+    public ListView msgView;
+    Assistant assistant;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -64,15 +83,56 @@ public class ChatFragment extends Fragment {
         mMessageAdapter = new MessageListAdapter(view.getContext(), mMessageList);
         mMessageRecycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
         mMessageRecycler.setAdapter(mMessageAdapter);
+
+        input = (EditText) view.findViewById(R.id.edittext_chatbox);
         ImageButton sendMessage = (ImageButton) view.findViewById(R.id.button_chatbox_send);
         new DBConnect().execute("");
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(view.getContext(), "Clicked on chat", Toast.LENGTH_LONG).show();
+                String inputText = input.getText().toString();
+                new WatsonTask().execute(inputText);
+
             }
         });
         return view;
+    }
+
+    public static MessageResponse startService(String assistant_apikey, String assistant_url, String workspace_id){
+        IamAuthenticator authenticator = new IamAuthenticator(assistant_apikey);
+        Assistant assistant = new Assistant("2019-04-30", authenticator);
+        assistant.setServiceUrl(assistant_url);
+        CreateSessionOptions createSessionOptions =
+                new CreateSessionOptions.Builder().assistantId(workspace_id).build();
+        createSessionOptions = createSessionOptions.newBuilder().build();
+        MessageOptions messageOptions =
+                new MessageOptions.Builder().assistantId(workspace_id).sessionId("session_id").build();
+        MessageResponse response = assistant.message(messageOptions).execute().getResult();
+        System.out.println("Checking: " + response);
+        return response;
+    }
+
+    public void displayMsg(MessageResponse msg) {
+        final MessageResponse mssg=msg;
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                //from the WCS API response
+                //extract the text from output to display to the user
+                String text = mssg.getOutput().toString();//.get(0);
+
+                //now output the text to the UI to show the chat history
+                msgList.add(text);
+                msgView.setAdapter(msgList);
+                msgView.smoothScrollToPosition(msgList.getCount() - 1);
+
+                //set the context, so that the next time we call WCS we pass the accumulated context
+                context = mssg.getContext();
+            }
+        });
+
     }
 
     public List<Message> getMessageList(){
@@ -126,6 +186,22 @@ public class ChatFragment extends Fragment {
 
         protected void onPostExecute(String feed) {
             //Toast.makeText(view.getContext(), feed, Toast.LENGTH_LONG).show();
+        }
+    }
+    class WatsonTask extends AsyncTask {
+
+        private Exception exception;
+
+        protected void onPostExecute(MessageResponse feed) {
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+
+        @Override
+        protected MessageResponse doInBackground(Object[] objects) {
+            MessageResponse messageResponse = WatsonUtils.startService(assistant_apikey,assistant_url,workspace_id, objects[0].toString());
+            System.out.println("Response: " + messageResponse.toString());
+            return messageResponse;
         }
     }
 }
--- app/src/main/java/com/cotrack/utils/WatsonUtils.java
+++ app/src/main/java/com/cotrack/utils/WatsonUtils.java
@@ -1,7 +1,17 @@
 package com.cotrack.utils;
 
 import com.ibm.cloud.sdk.core.http.Response;
+import com.ibm.cloud.sdk.core.security.Authenticator;
 import com.ibm.cloud.sdk.core.security.BasicAuthenticator;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.assistant.v2.Assistant;
+import com.ibm.watson.assistant.v2.model.CreateSessionOptions;
+import com.ibm.watson.assistant.v2.model.DeleteSessionOptions;
+import com.ibm.watson.assistant.v2.model.MessageInput;
+import com.ibm.watson.assistant.v2.model.MessageOptions;
+import com.ibm.watson.assistant.v2.model.MessageResponse;
+import com.ibm.watson.assistant.v2.model.RuntimeResponseGeneric;
+import com.ibm.watson.assistant.v2.model.SessionResponse;
 import com.ibm.watson.natural_language_understanding.v1.NaturalLanguageUnderstanding;
 import com.ibm.watson.natural_language_understanding.v1.model.AnalysisResults;
 import com.ibm.watson.natural_language_understanding.v1.model.AnalyzeOptions;
@@ -9,9 +19,12 @@ import com.ibm.watson.natural_language_understanding.v1.model.EntitiesOptions;
 import com.ibm.watson.natural_language_understanding.v1.model.Features;
 import com.ibm.watson.natural_language_understanding.v1.model.KeywordsOptions;
 
+import java.util.List;
+import java.util.logging.LogManager;
+
 public class WatsonUtils {
-    public static final String SERVICE = "2018-03-16";
-    public static final String VERSION_DATE = "service";
+    public static final String SERVICE = "service";
+    public static final String VERSION_DATE = "2018-03-16";
     public static final String USERNAME = "";
     public static final String PASSWORD = "";
 
@@ -60,6 +73,44 @@ public class WatsonUtils {
         //print the result
         System.out.println(response);
         return response.toString();
+
     }
 
+    public static MessageResponse startService(String assistant_apikey, String assistant_url, String workspace_id, String inputText) {
+        // Suppress log messages in stdout.
+        LogManager.getLogManager().reset();
+
+        // Set up Assistant service.
+        Authenticator authenticator = new IamAuthenticator(assistant_apikey); // replace with API key
+        Assistant service = new Assistant("2019-02-28", authenticator);
+        //service.setServiceUrl(assistant_url);
+        String assistantId = workspace_id; // replace with assistant ID
+
+        // Create session.
+        CreateSessionOptions createSessionOptions = new CreateSessionOptions.Builder(assistantId).build();
+        SessionResponse session = service.createSession(createSessionOptions).execute().getResult();
+        String sessionId = session.getSessionId();
+        // Initialize with empty message to start the conversation.
+        MessageInput input = new MessageInput.Builder()
+                .messageType("text")
+                .text(inputText)
+                .build();
+        // Start conversation with empty message.
+        MessageOptions messageOptions = new MessageOptions.Builder(assistantId,
+                sessionId).input(input).build();
+        MessageResponse response = service.message(messageOptions).execute().getResult();
+
+        // Print the output from dialog, if any. Assumes a single text response.
+        List<RuntimeResponseGeneric> responseGeneric = response.getOutput().getGeneric();
+        if (responseGeneric.size() > 0) {
+            if (responseGeneric.get(0).responseType().equals("text")) {
+                System.out.println(responseGeneric.get(0).text());
+            }
+        }
+        // We're done, so we delete the session.
+        DeleteSessionOptions deleteSessionOptions = new DeleteSessionOptions.Builder(assistantId, sessionId).build();
+        service.deleteSession(deleteSessionOptions).execute();
+        return response;
+
+    }
 }
