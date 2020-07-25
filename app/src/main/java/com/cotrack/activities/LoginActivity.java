0abb3cedde4c2ec8ecb9245aaa40bae82cf211ac
aritra0925 <aritra0925@github.com>
2020-07-26 00:10:43 +0530

    Adding service code

--- app/src/main/java/com/cotrack/activities/LoginActivity.java
+++ app/src/main/java/com/cotrack/activities/LoginActivity.java
@@ -73,8 +73,8 @@ public class LoginActivity extends AccountAuthenticatorActivity {
     @BindView(R.id.login_user_type)
     RadioGroup _radioGroup;
     boolean flag = false;
-    String[] PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE};
-    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION };
+    int PERMISSION_ALL = 101;
     String email;
     LoginService mLoginService;
     Intent mServiceIntent;
@@ -115,25 +115,43 @@ public class LoginActivity extends AccountAuthenticatorActivity {
                 startActivityForResult(intent, REQUEST_SIGNUP);
             }
         });
-        if (!hasPermissions(this, PERMISSIONS)) {
-            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
-            System.out.println("Checking permission request");
-        }
-        if(session.getusername() != null && !session.getusername().isEmpty()
+        askFotLocationPermission();
+        if (session.getusername() != null && !session.getusername().isEmpty()
                 && session.getUserType() != null && !session.getUserType().isEmpty()) {
             email = session.getusername();
             onSessionActive();
         }
     }
 
+    public void askFotLocationPermission(){
+        boolean foreground = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        if (foreground) {
+            boolean background = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+            if (background) {
+                //handleLocationUpdates();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, PERMISSION_ALL);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION}, PERMISSION_ALL);
+        }
+    }
+
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        String userCookie =  savedInstanceState.getString(USER_COOKIE);
-        if(!TextUtils.isEmpty(userCookie)) {
+        String userCookie = savedInstanceState.getString(USER_COOKIE);
+        if (!TextUtils.isEmpty(userCookie)) {
             this.userCookie = userCookie;
         }
-        String userTypeCookie =  savedInstanceState.getString(USER_TYPE_COOKIE);
-        if(!TextUtils.isEmpty(userTypeCookie)) {
+        String userTypeCookie = savedInstanceState.getString(USER_TYPE_COOKIE);
+        if (!TextUtils.isEmpty(userTypeCookie)) {
             this.userTypeCookie = userTypeCookie;
         }
         super.onRestoreInstanceState(savedInstanceState);
@@ -220,11 +238,11 @@ public class LoginActivity extends AccountAuthenticatorActivity {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
             if (serviceClass.getName().equals(service.service.getClassName())) {
-                Log.i ("Service status", "Running");
+                Log.i("Service status", "Running");
                 return true;
             }
         }
-        Log.i ("Service status", "Not running");
+        Log.i("Service status", "Not running");
         return false;
     }
 
@@ -399,22 +417,6 @@ public class LoginActivity extends AccountAuthenticatorActivity {
         }
     }
 
-    public boolean hasPermissions(Context context, String... permissions) {
-        if (context != null && permissions != null) {
-            for (String permission : permissions) {
-                int selfPermission = ActivityCompat.checkSelfPermission(context, permission);
-                int permissionGrantedCode = PackageManager.PERMISSION_GRANTED;
-                System.out.println("Permission status: " + selfPermission);
-                System.out.println("Permission Granted: " + permissionGrantedCode);
-                if (selfPermission != permissionGrantedCode) {
-                    System.out.println("App permmission not working");
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -423,11 +425,16 @@ public class LoginActivity extends AccountAuthenticatorActivity {
             // for each permission check if the user granted/denied them
             // you may want to group the rationale in a single dialog,
             // this is just an example
+            boolean flag = false;
             for (int i = 0, len = permissions.length; i < len; i++) {
                 String permission = permissions[i];
                 if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
                     // user rejected the permission
-                    showExplanation("Permission Required", "This app will not work without the permissions", permission, PERMISSION_ALL);
+                    if(!flag) {
+                        showExplanation("Permission Required", "In order to keep you safe we need to know you location even when you are not using the app. " +
+                                "Please note that your location data will not be made public. This is for our use only", permission, PERMISSION_ALL);
+                    }
+                    flag = true;
                     boolean showRationale = shouldShowRequestPermissionRationale(permission);
                     if (!showRationale) {
                         // user also CHECKED "never ask again"
@@ -463,17 +470,17 @@ public class LoginActivity extends AccountAuthenticatorActivity {
                 PERMISSIONS, permissionRequestCode);
     }
 
-    public Properties writeProperties(String key, String value){
+    public Properties writeProperties(String key, String value) {
         Properties props = new Properties();
         try {
             File file = this.getFileStreamPath(COOKIE_FILE_NAME);
-            if(!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
-            FileInputStream  fin= openFileInput(COOKIE_FILE_NAME);
+            FileInputStream fin = openFileInput(COOKIE_FILE_NAME);
             props.load(fin);
             props.put(key, value);
-            FileOutputStream fOut = openFileOutput(COOKIE_FILE_NAME,Context.MODE_PRIVATE);
+            FileOutputStream fOut = openFileOutput(COOKIE_FILE_NAME, Context.MODE_PRIVATE);
             props.store(fOut, "Cookie Data");
             System.out.println("Properties was written successfully");
         } catch (FileNotFoundException e) {
--- app/src/main/java/com/cotrack/fragments/HomeFragment.java
+++ app/src/main/java/com/cotrack/fragments/HomeFragment.java
@@ -1,8 +1,13 @@
 package com.cotrack.fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -10,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,12 +28,16 @@ import com.cotrack.models.Country;
 import com.cotrack.models.Countries;
 import com.cotrack.models.Global;
 import com.cotrack.utils.JSONUtils;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONObject;
 
@@ -37,8 +47,11 @@ import org.json.JSONObject;
  * create an instance of this fragment.
  */
 @SuppressWarnings("ALL")
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    final int REQUEST_CODE = 101;
     public static final String COVID_19_URL = "https://api.covid19api.com/summary";
 
     /**
@@ -73,6 +86,8 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
         setUpMapIfNeeded();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(view.getContext());
+        fetchLocation(view.getContext());
         doGetCovidSummary(view.getContext());
         return view;
     }
@@ -83,15 +98,12 @@ public class HomeFragment extends Fragment {
         if (mMapFragment == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMapFragment = ((SupportMapFragment) getParentFragmentManager().findFragmentById(R.id.map));
-            // Check if we were successful in obtaining the map.
-            if (mMapFragment != null) {
-                setUpMap();
-            }
+
         }
 
     }
 
-    private void setUpMap() {
+    /*private void setUpMap() {
         mMapFragment.getMapAsync(new OnMapReadyCallback(){
             @Override
             public void onMapReady(GoogleMap googleMap) {
@@ -104,7 +116,7 @@ public class HomeFragment extends Fragment {
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
             }
         });
-    }
+    }*/
 
     public void doGetCovidSummary(Context context) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
@@ -147,5 +159,47 @@ public class HomeFragment extends Fragment {
         );
         requestQueue.add(jsonObjectRequest);
 
+    }
+
+    private void fetchLocation(Context context) {
+        if (ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(context.getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+                    assert supportMapFragment != null;
+                    supportMapFragment.getMapAsync(HomeFragment.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here!");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation(getContext());
+                }
+                break;
+        }
     }
 }
